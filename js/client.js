b3147cf87a25c1edfa9f954f454713327f07e5ab
sbagal01 <bagals425@gmail.com>
2023-04-22 18:29:33 +0530

    update12345

--- js/client.js
+++ js/client.js
@@ -1,5 +1,5 @@
-// const socket=io("http://localhost:8000");
-const socket=io("https://pranav15061.github.io/chat1/:8000");
+const socket=io("http://localhost:8000");
+// const socket=io("https://pranav15061.github.io/chat1/:8000");
 
 
 const form=document.getElementById("send-container");
